5fa121c1c87bb4773e67b191128e59caf044b19f
aka1988 <54412539+aka1988@users.noreply.github.com>
2020-10-27 21:50:25 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function(e) {
+
+	
+	// $(".pro_search1").hide(0);
+    $(".button3").click(
+		function(){
+			$(this).next(".pro_search1").slideToggle(500);	
+		}
+	)
+});
